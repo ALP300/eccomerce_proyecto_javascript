dce1e7a67fdf063cc321cda28f31cae578907bdc
ALP300 <aitorpalomino1@gmail.com>
2026-04-28 21:27:14 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import { conectar } from "./config/database.js";
+import express from "express";
+const app = express();
 
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+});
 conectar();
