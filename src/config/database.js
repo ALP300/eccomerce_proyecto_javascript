f4514a4b18ef91aaddb8c761f8a9e48221c3e0ec
ALP300 <aitorpalomino1@gmail.com>
2026-04-28 20:44:51 -0500

    a

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Pool, Client } from "pg";
 
 export const Pool = new Pool({
-    user: "db_javascript_user",
-    password: "1Ny8YJ4H60RKfLGweNHWc7DQV2T3FcRM",
-    host: "dpg-d7olpd9o3t8c7383b0gg-a.oregon-postgres.render.com",
-    port: 5432,
-    database: "db_javascript",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     ssl:{
         rejectUnauthorized: false
     }
